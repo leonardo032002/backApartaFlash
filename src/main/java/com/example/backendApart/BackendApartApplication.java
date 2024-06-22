06193763855d2162592b919de95d6b99c1e11e2e
leonardo martinez <leonardo.2002@live.com>
2024-06-22 13:52:00 -0500

    Nuevo Backaparflash

---
+++ src/main/java/com/example/backendApart/BackendApartApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendApart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApartApplication.class, args);
+	}
+
+}
