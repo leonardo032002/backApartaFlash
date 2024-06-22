ef3533339f6bc288317c05f4ec15fbb3086d1da0
Sandro <sbarroscaballero0826@gmail.com>
2024-06-22 14:18:02 -0500

    Cliente Completado Falta Relaciones

--- src/main/java/com/example/backendApart/controller/ClienteController.java
+++ src/main/java/com/example/backendApart/controller/ClienteController.java
@@ -1,4 +1,49 @@
 package com.example.backendApart.controller;
 
+import com.example.backendApart.entity.Cliente;
+import com.example.backendApart.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cliente")
+@CrossOrigin(origins = "*")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping("/")
+    public ResponseEntity<Boolean> guardarCliente(@RequestBody Cliente cliente) {
+        clienteService.crearCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Boolean.TRUE);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> traerClientePorId(@PathVariable Long id) {
+        Cliente clienteId = clienteService.traerClientePorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(clienteId);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Cliente>> traerClientes() {
+        List<Cliente> listaCliente = clienteService.traerClientes();
+        return ResponseEntity.status(HttpStatus.OK).body(listaCliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> actualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        clienteService.actualizarCliente(id, cliente);
+        return ResponseEntity.status(HttpStatus.OK).body(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> eliminarCliente(@PathVariable Long id) {
+        clienteService.eliminarCliente(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- src/main/java/com/example/backendApart/service/ClienteService.java
+++ src/main/java/com/example/backendApart/service/ClienteService.java
@@ -5,6 +5,7 @@ import com.example.backendApart.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,6 +16,31 @@ public class ClienteService {
 
     public void crearCliente(Cliente cliente) {
         clienteRepository.save(cliente);
+    }
+
+    public Cliente traerClientePorId(Long id) {
+        Optional<Cliente> clienteId = clienteRepository.findById(id);
+        if (clienteId.isPresent()) {
+            return clienteRepository.getReferenceById(id);
+        }
+        throw new RuntimeException("No existe el id");
+    }
+
+    public List<Cliente> traerClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente actualizarCliente(Long id, Cliente cliente) {
+        Optional<Cliente> clienteExistente = clienteRepository.findById(id);
+        if (clienteExistente.isPresent()) {
+            Cliente clienteActualizado = clienteExistente.get();
+
+            clienteActualizado.setNombre(cliente.getNombre());
+            clienteActualizado.setApellido(cliente.getApellido());
+            clienteActualizado.setCedula(cliente.getCedula());
+            return clienteRepository.save(clienteActualizado);
+        }
+        throw new RuntimeException("No existe el id");
     }
 
     public void eliminarCliente(Long id) {
